ce8c93f47e55e236f747a958b875da52e36336c8
Julianne Costa <julesleighcosta@gmail.com>
2017-04-13 13:37:32 -0700

    Complete 02, 03

--- MovieBuff/skeleton/movie_buff/03_queries.rb
+++ MovieBuff/skeleton/movie_buff/03_queries.rb
@@ -1,23 +1,46 @@
 def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
+  Movie
+    .select("movies.title, movies.id")
+    .joins(:actors)
+    .where("actors.name IN (?)", those_actors)
+    .group("movies.id")
+    .having("COUNT(*) >= (?)", those_actors.length)
 
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
-
+  Movie
+    .group("yr / 10")
+    .order("AVG(score) DESC")
+    .pluck("(yr / 10) * 10").first
 end
 
 def costars(name)
   # List the names of the actors that the named actor has ever appeared with.
   # Hint: use a subquery
 
+  movies = Movie
+  .joins(:actors)
+  .where("actors.name = (?)", name)
+  .pluck("movies.id")
+
+  Actor
+    .joins(:castings)
+    .where("castings.movie_id IN (?) AND actors.name != (?)", movies, name)
+    .pluck("DISTINCT(actors.name)")
+
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
-
+  Actor
+    .joins("LEFT OUTER JOIN castings ON castings.actor_id = actors.id")
+    .where("castings.actor_id IS NULL")
+    .pluck("COUNT(*)")
+    .first
 end
 
 def starring(whazzername)
@@ -26,6 +49,10 @@ def starring(whazzername)
   # ignoring case, in order.
 
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but not like "stallone sylvester" or "zylvester ztallone"
+  whazzername = "%" + whazzername.split("").join("%") + "%"
+  Movie
+    .joins(:actors)
+    .where("UPPER(actors.name) LIKE UPPER(?)", whazzername)
 
 end
 
